1445c99180c2ebddc63db71b6b240431c8668fed
nancy mandavia <nancymandavia.126465@marwadiuniversity.ac.in>
2026-07-03 08:48:54 +0530

    Add files via upload
    nancy mandavia

---
+++ string2.php
@@ -0,0 +1,73 @@
+<?php
+// String functions are built-in functions
+
+$str1 = "   NANCY";
+$str2 = "MANDAVIA";
+
+echo $str1 . " " . $str2;
+echo "<br>";
+
+print($str1 . " " . $str2);
+echo "<br>";
+
+echo strlen($str1 . $str2);
+
+echo "<br>";
+
+$trimstring = trim($str1);
+
+echo strlen($trimstring);
+
+echo "<br>";
+
+echo strrev($str1);
+
+echo "<br>";
+
+echo strtolower($str1);
+
+echo "<br>";
+
+echo strtoupper($str1);
+
+echo "<br>";
+
+echo strcmp("hello", "world");
+
+
+echo "<br>";
+echo strstr($str1,"A");
+
+echo  "<br>";
+echo stristr($str1,"a");
+
+echo "<br>";
+echo ucfirst("i am student in MU");
+
+echo "<br>";
+echo ucwords("i am a student in mu");
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
