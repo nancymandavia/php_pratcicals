5333210b14a9348547c62c758403fd85ab8d1387
nancy mandavia <nancymandavia.126465@marwadiuniversity.ac.in>
2026-07-17 08:46:46 +0530

    Add files via upload

---
+++ session_1.php
@@ -0,0 +1,14 @@
+<?php
+//Starting the session
+session_start();
+?>
+<html>
+    <body>
+        <!--After Login Process-->
+        <?php
+        $_SESSION['username']="nancy";
+        
+        // it generates a cookie
+        ?>
+        </body>
+        </html>
